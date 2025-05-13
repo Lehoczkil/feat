31f23175c61319735cad3c22635437ffb3190d4f
laszlo.lehoczki <laszlo.lehoczki@webpont.com>
2025-05-13 18:28:31 +0200

    chore: Setup project structure

---
+++ src/i18n/index.ts
@@ -0,0 +1,14 @@
+import { createI18n } from 'vue-i18n'
+import en from '../locales/en.json'
+import hu from '../locales/hu.json'
+
+export const i18n = createI18n({
+  legacy: false, // composition API támogatás
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages: {
+    en,
+    hu
+  },
+  globalInjection: true // így nem kell minden komponensbe importálni a t() függvényt
+})
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import { createPinia } from 'pinia'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { i18n } from './i18n'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia).use(i18n).mount('#app')
